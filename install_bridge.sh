90d03021c608a5461339a1542c9936f67fc56c3f
momefilo <106985138+momefilo@users.noreply.github.com>
2026-09-21 18:09:08 +0200

    Update install_bridge.sh

--- install_bridge.sh
+++ install_bridge.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
+#  !!!!!!!!! LOCALE MUESSEN IN RASPI-CONFIG GESETZT SEIN !!!!!!
 sudo apt update
 #sudo apt upgrade
 sudo apt install automake autoconf build-essential texinfo libtool libftdi-dev libusb-1.0-0-dev git minicom -y
-git clone https://github.com/raspberrypi/openocd.git --branch rp2040 --recursive --depth=1
+git clone https://github.com/raspberrypi/openocd.git --recursive --depth=1
 cd openocd
 ./bootstrap
-./configure --enable-ftdi --enable-sysfsgpio --enable-bcm2835gpio
+git submodule init
+git submodule update
+./configure --enable-ftdi --enable-sysfsgpio --enable-bcm2835gpio --enable-internal-jimtcl
+# !! Entferne in Makefil Zeile 1353 -Werror am Ende !!!
 make
 sudo make install
 # erstelle die ausfuehrbare Datei transfer.sh
